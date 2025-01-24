d46819fcaf76efead0482c7d6da0a899bfafddc3
Adam Abouljoud <62528589+adamabljd@users.noreply.github.com>
2025-01-24 11:44:41 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-red-500 flex flex-row'>
+        <div className='flex flex-col'>
+          <div>
+            History
+          </div>
+          <div>
+
+          </div>
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
